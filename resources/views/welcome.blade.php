06ccfcafca00dee06c231bd3a2add7dc9e94adb0
MikeVP05 <vertilix0@gmail.com>
2024-06-05 22:04:15 +0200

    Update add banner and change navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,16 @@
         <title>Laravel</title>
         <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
         <link rel="stylesheet" type="text/css" href="{{ asset('css/navbar.css') }}" >
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/index.css') }}" >
     </head>
     <body>
         <div class="navbar">
             <a class="navbar-brand" href="/">RNG</a>
             <div class="navbar-hrefs">
-                <a class="navbar-href" href="/">RNG</a>
+                <a class="navbar-href" href="/">Login</a>
             </div>
         </div>
+
+        <div class="banner"></div>
     </body>
 </html>
