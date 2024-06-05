f4b208f40f6e16cf65a88ac8aaeeed1f22a0775f
Mike <vertilix0@gmail.com>
2024-06-05 21:33:22 +0200

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" >
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/navbar.css') }}" >
+    </head>
+    <body>
+        <div class="navbar">
+            <a class="navbar-brand" href="/">RNG</a>
+            <div class="navbar-hrefs">
+                <a class="navbar-href" href="/">RNG</a>
+            </div>
+        </div>
+    </body>
+</html>
